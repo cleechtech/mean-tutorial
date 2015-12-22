dda011f69f28b3434c64d638abd98a24f549f9e0
Connor Leech <connorleech@gmail.com>
2015-12-21 19:14:26 -0800

    add express-stormpath

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 var express = require('express');
+var stormpath = require('express-stormpath');
+var path = require('path');
 var app = express();
 
+// express config
 app.use(express.static(__dirname + '/public'));
+app.use(stormpath.init(app, {
+  website: true,
+  web: {
+    spaRoot: path.join(__dirname, 'public', 'index.html')
+  }
+}));
 
+// routes
 app.get('/*', function(req, res){
 	res.sendFile(__dirname + '/public/index.html');
 });
 
-app.listen(3000, function(){
+// start server, wait for stormpath
+app.on('stormpath.ready', function () {
+  app.listen(3000, function(){
 	console.log('Server started on port ' + 3000);
+});
 });
